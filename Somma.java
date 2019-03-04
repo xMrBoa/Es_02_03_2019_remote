f21a0508f41d6c95592dc03987ccb138394e90ad
Niccolò Boanini <studente.boanini.niccolo@gmail.com>
2019-03-04 16:21:53 +0100

    Eliminato file java Hello, Creato file java somma, aggiunto Hello cpp

---
+++ Somma.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public static void main (Strings args[])
+{
+	int a, b, somma;
+	
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Inserisci primo numero: ");
+	a = scan.nextInt;
+	System.out.print("Inserisci primo numero: ");
+	b = scan.nextInt;
+	
+	somma = a + b;
+	
+	System.out.print("La sommma e': " + somma );
+}
